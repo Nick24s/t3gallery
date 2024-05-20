3a408791b0291eefb6404ee8369744a384383f45
Nick24s <nickmilenov@gmail.com>
2024-05-20 16:50:11 +0300

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { log } from "console";
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   'https://utfs.io/f/c2cb6e17-9d26-41e6-90d5-b1f151b2e68f-2oy3zh.png',
@@ -11,12 +13,21 @@ const mockImages = mockUrls.map((url,index) => ({
   url 
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const  posts = await db.query.posts.findMany();
+  console.log(posts);
+  
   return (
     <main className=''>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages,...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>
+            {post.name}
+          </div>
+        ))}
+        {[...mockImages,...mockImages].map((image , index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
           </div>
         ))}
